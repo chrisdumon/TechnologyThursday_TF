e367a2249b245ef4fa26c9e0cf8264792e139591
Christophe Dumont <christophe.dumont@lucidus.be>
2021-09-27 14:36:22 +0200

    Added working example of a simple VM instance on AWS

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 3.60.0"
     }
   }
@@ -10,7 +10,28 @@ terraform {
 }
 
 provider "aws" {
-  region      = var.region
-  access_key  = var.access_key
-  secret_key  = var.secret_key
+  region     = "eu-west-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 }
