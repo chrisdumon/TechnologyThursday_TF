5b67458daf30a16ce97927e12221deb98afb2b8a
Jens <jens@jensvde.be>
2021-09-29 15:26:10 +0200

    VM With Securitygroups and VPC demo

--- README.md
+++
@@ -1,7 +0,0 @@
-# TechnologyThursday_TF
-
-Code repository for demo given during the Bewire Technology Thursday edition about Terraform.
-
-Contributed by:
-- Christophe Dumont
-- Jens Van den Eynde
--- VMOnly/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  description = "The AWS access key, required in order to deploy resources on Amazon Web Services"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "The AWS secret key, required in order to deploy resources on Amazon Web Services"
-  type        = string
-}
